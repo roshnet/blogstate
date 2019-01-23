437b5d3860bfba19e735e75b88292d933f3f25c4
Roshan Sharma <reprogram46@gmail.com>
2019-01-24 00:02:37 +0530

    Initial setup (bare minimum)

---
+++ app.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template('index.html',
+							title='Home')
+
+
+if __name__ == '__main__':
+	# In dev mode:
+	app.run(debug=True)
