5cd89d3fd8ccc27204faf4db526022607f4e264d
Roshan Sharma <reprogram46@gmail.com>
2019-01-26 22:09:58 +0530

    app.py: Remove unnecessary argument in call to index.html

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html',
-							title='Home')
+	return render_template('index.html')
 
 
 if __name__ == '__main__':
